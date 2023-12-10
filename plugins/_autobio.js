a0c6f424356f9a9c679e873b937fee89b34464e2
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-12-10 06:34:05 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `🧚‍♂️𝐓𝐇𝐄 𝐐𝐔𝐄𝐄𝐍-𝐒𝐀𝐊𝐔𝐑𝐀_𝐌𝐃 𝐕2 𝐕𝐄𝐑𝐒𝐈𝐎𝐍 𝐁𝐎𝐓 𝐑𝐔𝐍 𝐓𝐈𝐌𝐄 ${muptime} 𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃 𝐁𝐘 𝐒𝐘𝐁𝐄𝐑-𝐃𝐄𝐌𝐎𝐍𝐒 𝐓𝐄𝐀𝐌....`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' 📅𝐃𝐀𝐘𝐒 ️', h, ' 🕗𝐇𝐎𝐔𝐑 ', m, ' ⏳𝐌𝐈𝐍𝐔𝐓𝐄𝐒'].map(v => v.toString().padStart(2, 0)).join('')
 }
